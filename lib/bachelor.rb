eb9c8ea434b872c3063b6dcb98d33b954ee5b2ae
Conrad Casper <conradcasper@icloud.com>
2019-06-04 13:01:56 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,10 +1,5 @@
 require "pry"
-def find_contestant(data, name)
-  data.each do |season, contestants|
-    binding.pry
-  end
-end
-  
+
 
 def get_first_name_of_season_winner(data, season)
   data.each do |season, contestants|
